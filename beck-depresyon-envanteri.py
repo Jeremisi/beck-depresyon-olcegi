fb23d3b92f394fccedcf156d7da3edc57e648955
Goncagül Kemancı <106958079+Jeremisi@users.noreply.github.com>
2023-02-01 10:13:03 +0300

    Add files via upload

---
+++ beck-depresyon-envanteri.py
@@ -0,0 +1,211 @@
+print("Bu form son bir (1) hafta içerisinde kendinizi nasıl hissettiğinizi araştırmaya yönelik 21 maddeden oluşmaktadır. ")
+print("Her maddenin karşısındaki dört cevabı dikkatlice okuduktan sonra, size en çok uyan, yani sizin durumunuzu en iyi anlatanı işaretlemeniz gerekmektedir.")
+
+#1
+print("(0) Üzüntülü ve sıkıntılı değilim ")
+print("(1) Kendimi üzüntülü ve sıkıntılı hissediyorum.")
+print("(2) Hep üzüntülü ve sıkıntılıyım. Bundan kurtulamıyorum")
+print("(3) O kadar üzgün ve sıkıntılıyım ki, artık dayanamıyorum.")
+
+x1 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x2 = x1
+
+#2
+print("(0) Gelecek hakkında umutsuz ve karamsar değilim.")
+print("(1) Gelecek için karamsarım.")
+print("(2) Gelecekten beklediğim hiçbir şey yok.")
+print("(3) Gelecek hakkında umutsuzum ve sanki hiçbir şey düzelmeyecekmiş gibi geliyor.")
+
+x3 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x4 = x3
+
+#3
+print("(0) Kendimi başarısız biri olarak görmüyorum.")
+print("(1) Başkalarından daha başarısız olduğumu hissediyorum.")
+print("(2) Geçmişe baktığımda başarısızlıklarla dolu olduğunu görüyorum.")
+print("(3) Kendimi tümüyle başarısız bir insan olarak görüyorum.")
+
+x5 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x6 = x5
+
+#4
+print("(0) Her şeyden eskisi kadar zevk alıyorum.")
+print("(1) Birçok şeyden eskiden olduğu gibi zevk alamıyorum.")
+print("(2) Artık hiçbir şey bana tam anlamıyla zevk vermiyor.")
+print("(3) Her şeyden sıkılıyorum.")
+
+x7 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x8 = x7
+
+#5
+print("(0) Kendimi herhangi bir biçimde suçlu hissetmiyorum.")
+print("(1) Kendimi zaman zaman suçlu hissediyorum.")
+print("(2) Çoğu zaman kendimi suçlu hissediyorum.")
+print("(3) Kendimi her zaman suçlu hissediyorum.")
+
+x9 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x10 = x9
+
+#6
+print("(0) Kendimden memnunum.")
+print("(1) Kendimden pek memnun değilim.")
+print("(2) Kendime kızgınım.")
+print("(3) Kendimden nefrete ediyorum.")
+
+x11 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x12 = x11
+
+#7
+print("(0) Başkalarından daha kötü olduğumu sanmıyorum.")
+print("(1) Hatalarım ve zayıf taraflarım olduğunu düşünüyorum.")
+print("(2) Hatalarımdan dolayı kendimden utanıyorum.")
+print("(3) Herşeyi yanlış yapıyormuşum gibi geliyor ve hep kendimi kabahatli buluyorum")
+
+x13 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x14 = x13
+
+
+#8
+print("(0) Kendimi öldürmek gibi düşüncülerim yok.")
+print("(1) Kimi zaman kendimi öldürmeyi düşündüğüm oluyor ama yapmıyorum.")
+print("(2) Kendimi öldürmek isterdim.")
+print("(3) Fırsatını bulsam kendimi öldürürüm.")
+
+x15 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x16 = x15
+
+#9
+print("(0) Kendimi öldürmek gibi düşüncülerim yok.")
+print("(1) Kimi zaman kendimi öldürmeyi düşündüğüm oluyor ama yapmıyorum.")
+print("(2) Kendimi öldürmek isterdim.")
+print("(3) Fırsatını bulsam kendimi öldürürüm.")
+
+x17 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x18 = x17
+
+#10
+print("(0) Her zaman olduğumdan daha canı sıkkın ve sinirli değilim.")
+print("(1) Eskisine oranla daha kolay canım sıkılıyor ve kızıyorum.")
+print("(2) Her şey canımı sıkıyor ve kendimi hep sinirli hissediyorum.")
+print("(3) Canımı sıkan şeylere bile artık kızamıyorum.")
+
+x19 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x20 = x19
+
+#11
+print("(0) Başkalarıyla görüşme, konuşma isteğimi kaybetmedim.")
+print("(1) Eskisi kadar insanlarla birlikte olmak istemiyorum.")
+print("(2) Birileriyle görüşüp konuşmak hiç içimden gelmiyor")
+print("(3) Artık çevremde hiç kimseyi istemiyorum.")
+
+x21 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x22 = x21
+
+#12
+print("(0) Karar verirken eskisinden fazla güçlük çekmiyorum.")
+print("(1) Eskiden olduğu kadar kolay karar veremiyorum.")
+print("(2) Eskiye kıyasla karar vermekte çok güçlük çekiyorum.")
+print("(3) Artık hiçbir konuda karar veremiyorum.")
+
+x23 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x24 = x23
+
+#13
+print("(0) Her zamankinden farklı göründüğümü sanmıyorum.")
+print("(1) Aynada kendime her zamankinden kötü görünüyorum.")
+print("(2) Aynaya baktığımda kendimi yaşlanmış ve çirkinleşmiş buluyorum.")
+print("(3) Kendimi çok çirkin buluyorum")
+
+x25 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x26 = x25
+
+#14
+print("(0) Eskisi kadar iyi iş güç yapabiliyorum.")
+print("(1) Her zaman yaptığım işler şimdi gözümde büyüyor.")
+print("(2) Ufacık bir işi bile kendimi çok zorlayarak yapabiliyorum.")
+print("(3) Artık hiçbir iş yapamıyorum.")
+
+x27 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x28 = x27
+
+
+#15
+print("(0) Uykum her zamanki gibi.")
+print("(1) Eskisi gibi uyuyamıyorum.")
+print("(2) Her zamankinden 1-2 saat önce uyanıyorum ve kolay kolay tekrar uykuya dalamıyorum")
+print("(3) Sabahları çok erken uyanıyorum ve bir daha uyuyamıyorum.")
+
+x29 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x30 = x29
+
+
+#16
+print("(0) Kendimi her zamankinden yorgun hissetmiyorum.")
+print("(1) Eskiye oranla daha çabuk yoruluyorum.")
+print("(2) Her şey beni yoruyor.")
+print("(3) Kendimi hiçbir şey yapamayacak kadar yorgun ve bitkin hissediyorum")
+
+x31 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x32 = x31
+
+
+#17
+print("(0) İştahım her zamanki gibi.")
+print("(1) Eskisinden daha iştahsızım.")
+print("(2) İştahım çok azaldı.")
+print("(3) Hiçbir şey yiyemiyorum.")
+
+x33 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x34 = x33
+
+
+#18
+print("(0) Son zamanlarda zayıflamadım.")
+print("(1) Zayıflamaya çalışmadığım halde en az 2 Kg verdim.")
+print("(2) Zayıflamaya çalışmadığım halde en az 4 Kg verdim.")
+print("(3) Zayıflamaya çalışmadığım halde en az 6 Kg verdim.")
+
+x35 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x36 = x35
+
+#19
+print("(0) Sağlığımla ilgili kaygılarım yok.")
+print("(1) Ağrılar, mide sancıları, kabızlık gibi şikayetlerim oluyor ve bunlar beni tasalandırıyor.")
+print("(2) Sağlığımın bozulmasından çok kaygılanıyorum ve kafamı başka şeylere vermekte zorlanıyorum.")
+print("(3) Sağlık durumum kafama o kadar takılıyor ki, başka hiçbir şey düşünemiyorum.")
+
+x37 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x38 = x37
+
+#20
+print("(0) Sekse karşı ilgimde herhangi bir değişiklik yok.")
+print("(1) Eskisine oranla sekse ilgim az.")
+print("(2) Cinsel isteğim çok azaldı.")
+print("(3) Hiç cinsel istek duymuyorum.")
+
+x39 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x40 = x39
+
+#21
+print("(0) Cezalandırılması gereken şeyler yapığımı sanmıyorum.")
+print("(1) Yaptıklarımdan dolayı cezalandırılabileceğimi düşünüyorum.")
+print("(2) Cezamı çekmeyi bekliyorum.")
+print("(3) Sanki cezamı bulmuşum gibi geliyor.")
+
+x41 = int(input("Sizi en iyi tanımlayan cümlenin rakamını yazınız: "))
+x42 = x41
+
+result = [x2,x4,x6,x8,x10,x12,x14,x16,x18,x20,x22,x24,x26,x28,x30,x32,x34,x36,x38,x40,x42]
+
+result1= sum(result)
+
+if result1 <= 9:
+    print("Minimal şiddette depresyon belirtileri")
+elif 10 <= result1 <= 16:
+    print("Hafif şiddette depresyon belirtileri")
+elif 17 <= result1 <= 29:
+    print("Orta şiddette depresyon belirtileri")
+else:
+    print("Şiddetli düzeyde depresyon belirtileri")
+
+print("Beck Depresyon Envanteri puanınız: " + str(result1))
